3c6d61bc16716ae554ba43811a120795ad0ef4b7
sungil kang <68050246+madahari@users.noreply.github.com>
2024-10-14 01:24:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,10 @@ request_uri = ""
 # Show title and description.
 st.title("💬 Healthmate Chatbot")
 st.write(
-        "안녕하세요, 질병은 우리 모두를 힘들게 합니다.\n\n"
-        "이 봇의 목적은 기본적인 의료 정보와 조언을 제공함과 동시에 실제 의료 전문가의 진단을 받아 고통을 줄이고 더 큰 질병을 예방하는 것을 목표로 합니다.\n\n"
-        "궁금한 증상이 무엇인가요? 어떤 의료 조언을 받고 싶으신가요?"
-    )
-
+    "안녕하세요, 질병은 우리 모두를 힘들게 합니다.\n\n"
+    "이 봇의 목적은 기본적인 의료 정보와 조언을 제공함과 동시에 실제 의료 전문가의 진단을 받아 고통을 줄이고 더 큰 질병을 예방하는 것을 목표로 합니다.\n\n"
+    "궁금한 증상이 무엇인가요? 어떤 의료 조언을 받고 싶으신가요?"
+)
 
 # Create a session state variable to store the chat messages. This ensures that the
 # messages persist across reruns.
@@ -24,7 +23,7 @@ for message in st.session_state.messages:
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
 if prompt := st.chat_input("질문을 입력하세요."):
-   # Store and display the current prompt.
+    # Store and display the current prompt.
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -48,3 +47,34 @@ with st.chat_message("assistant"):
                 st.write("답변을 얻지 못했습니다.")
     except Exception:
         st.error("서버와 통신할 수 없습니다.")
+
+# 추천 검색어 링크
+st.write("추천 검색어:")
+st.markdown("[검색어1](https://hmchat.streamlit.app/?q=검색어1)")
+st.markdown("[검색어2](https://hmchat.streamlit.app/?q=검색어2)")
+st.markdown("[검색어3](https://hmchat.streamlit.app/?q=검색어3)")
+
+# 링크 클릭 시 질문 자동 입력 및 답변 생성
+query_params = st.experimental_get_query_params()
+if "q" in query_params:
+    question = query_params["q"][0]
+    st.session_state.messages.append({"role": "user", "content": question})
+    with st.chat_message("user"):
+        st.markdown(question)
+    api_url = "https://5c77-124-58-113-155.ngrok-free.app/chat/"
+    request_uri = api_url + question
+    with st.chat_message("assistant"):
+        try:
+            if request_uri:
+                data = requests.get(request_uri).json()
+                if data:
+                    response = data['message'].replace("~", "\~")
+                    display_text = response.split("참고문헌:")
+                    st.markdown(display_text[0])
+                    if len(display_text) > 1:
+                        st.info('**참고문헌:**' + display_text[1])
+                    st.session_state.messages.append({"role": "assistant", "content": response})
+                else:
+                    st.write("답변을 얻지 못했습니다.")
+        except Exception:
+            st.error("서버와 통신할 수 없습니다.")
